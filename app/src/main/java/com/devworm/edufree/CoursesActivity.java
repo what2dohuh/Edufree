32bb978429e7f9918440ae415d3b5569a705a9b0
Deep6999 <deepsharm17@gmail.com>
2021-11-07 18:47:08 +0530

    on going..

---
+++ app/src/main/java/com/devworm/edufree/CoursesActivity.java
@@ -0,0 +1,14 @@
+package com.devworm.edufree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+    }
+}
--- app/src/main/java/com/devworm/edufree/HomeFragment.java
+++ app/src/main/java/com/devworm/edufree/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.devworm.edufree;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,11 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
-
+CardView androiddev;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-
+        initial(v);
+        androiddev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),CoursesActivity.class));
+            }
+        });
         return v;
+    }
+
+    private void initial(View v) {
+        androiddev = v.findViewById(R.id.androiddev);
     }
 }
